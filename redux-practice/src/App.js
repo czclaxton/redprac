17a73ecb53c8080c7078e06eac81a5d5cdfd91bf
Connor Claxton <czclaxton@gmail.com>
2019-10-23 17:21:26 -0500

    set up initial boiler plate and hit dummy data post api

---
+++ redux-practice/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Posts from "./components/Posts";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Practice</header>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
---
+++ redux-practice/src/components/Posts.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => setPosts(res.data));
+  });
+
+  return (
+    <div>
+      {posts.map(post => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
